63f3861c17cd463fbc75c23559afddd321cc4cfd
sebasganba <91152492+sebasganba@users.noreply.github.com>
2022-05-01 18:27:42 +0200

    done with basics

--- 01-basic-python/08-sets/02-remove-duplicates/student.py
+++ 01-basic-python/08-sets/02-remove-duplicates/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+def remove_duplicates(xs):
+    result = set()
+    xx = []
+
+    for x in xs:
+        if x not in result:
+            xx.append(x)
+            result.add(x)
+    
+    return xx
+
